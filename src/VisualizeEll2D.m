9f241150f419a184e102f718eff62d0aa9b58fb1
MCMF <>
2017-10-12 11:16:27 +0200

    minor changes

--- src/VisualizeEll2D.m
+++ src/VisualizeEll2D.m
@@ -1,14 +1,16 @@
 function VisualizeEll2D(H, P, Color)
+
 z = P.Ell.z;
 a = P.Ell.a;
 b = P.Ell.b;
 alpha = P.Ell.g;
 f = P.Ell.pf;
 AB = P.Ell.AB;
+
 % Plot ellipses & foci in 2D
 scatter(H, z(1),z(2),'MarkerEdgeColor', [0,0,0], 'MarkerFaceColor',Color);
-Props.Color = Color;
-plotellipse(H, z, a, b, alpha, Props);
+ellProps.Color = Color;
+plotellipse(H, z, a, b, alpha, ellProps);
 quiver(H, repmat(z(1),2,1),repmat(z(2),2,1),AB(:,1),AB(:,2),...
     ':k','Autoscale','off','ShowArrowHead','off');
 scatter(H, f(:,1),f(:,2),'MarkerEdgeColor', [0,0,0], 'MarkerFaceColor',Color);
