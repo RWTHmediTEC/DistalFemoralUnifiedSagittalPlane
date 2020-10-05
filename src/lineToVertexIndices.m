79df5fed1888c1e732c8a403860905e8a4fa3549
MCMF <>
2020-10-05 17:37:45 +0200

    docu

--- src/lineToVertexIndices.m
+++ src/lineToVertexIndices.m
@@ -1,4 +1,6 @@
 function Idx = lineToVertexIndices(line, mesh)
+%LINETOVERTEXINDICES converts the representation of a line from origin and
+% direction vector to two vertex indices of a the mesh.
 %
 % AUTHOR: Maximilian C. M. Fischer
 % COPYRIGHT (C) 2020 Maximilian C. M. Fischer
@@ -7,8 +9,8 @@ function Idx = lineToVertexIndices(line, mesh)
 
 [linePts, linePos] = intersectLineMesh3d(line, mesh.vertices, mesh.faces);
 [linePts, linePtsIdx] = unique(linePts,'rows','stable');
-[~, linePosIdx]=sort(linePos(linePtsIdx));
-linePts=linePts(linePosIdx,:);
+[~, linePosIdx] = sort(linePos(linePtsIdx));
+linePts = linePts(linePosIdx,:);
 [~, Idx] = pdist2(mesh.vertices,[linePts(1,:);linePts(end,:)],'euclidean','Smallest',1);
 
 end
